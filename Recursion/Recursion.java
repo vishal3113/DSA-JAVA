764f1a4a27b0be820d7a499c6d9fc210bd0e4122
vishal tiwari <vishaltiwari13200@gmail.com>
2023-07-12 14:35:44 +0530

    Updated R2

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -12,17 +12,16 @@ public class Recursion {
     }
 
 public static void R2(int n){
-    int count=1;
-        if(n==count){
+    if(n==0){
             return;
         }
-        // Decrease Function
+        // Increase Function
+        R2(n-1);
         System.out.println(n);
-        R1(n+1);
     }
 
     public static void main(String[] args) {
         Recursion obj=new Recursion();
-        obj.R1(4);
+        obj.R2(4);
     }
 }
